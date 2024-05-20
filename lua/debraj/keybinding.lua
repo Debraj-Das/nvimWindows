824f0eda23b7f0a64764412539c59cd1cf8c879c
Debraj-Das <debrajdas.gayatri@gmail.com>
2024-05-21 01:26:28 +0530

    update some of my keybinding

--- lua/debraj/keybinding.lua
+++ lua/debraj/keybinding.lua
@@ -10,6 +10,8 @@ vim.keymap.set('n', '<C-j>', ':m+1<CR>', { noremap = true })
 vim.keymap.set('n', '<C-k>', ':m-2<CR>', { noremap = true })
 vim.keymap.set('n', 'd', '"_d', { noremap = true })
 vim.keymap.set('n', '<BS>', '"_xh', { noremap = true })
+vim.keymap.set("n", "n", "nzzzv", { noremap = true })
+vim.keymap.set("n", "N", "Nzzzv", { noremap = true })
 
 -- Visual mode key bindings
 vim.keymap.set('v', '<C-j>', ':m\'>+1<CR>gv=gv', { noremap = true })
@@ -25,14 +27,15 @@ vim.keymap.set('i', '{<CR>', '{<CR>}<esc>ko', { noremap = true })
 
 -- Leader key mappings
 vim.keymap.set('n', '<Leader>n',  vim.cmd.tabnew, { noremap = true })
-vim.keymap.set('n', '<Leader>q',  vim.cmd.q, {noremap = true })
 vim.keymap.set('n', '<Leader>j',	 vim.cmd.tabprevious, { noremap = true })
 vim.keymap.set('n', '<Leader>k',	 vim.cmd.tabnext, { noremap = true })
-vim.keymap.set('n', '<Leader>s',  vim.cmd.w, { noremap = true })
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gc<Left><Left><Left>]], {noremap = true})
 vim.keymap.set('n', '<Leader>y', '<esc>:%y+<CR>', { noremap = true })
 vim.keymap.set('n', '<Leader>v', '<C-v>', { noremap = true })
 vim.keymap.set('n', '<Leader>r', '<C-r>', { noremap = true })
 vim.keymap.set('n', '<Leader>h', ':set hlsearch!<CR>', { noremap = true })
+vim.keymap.set("n", "<Leader>d", "<C-d>zz", { noremap = true })
+vim.keymap.set("n", "<Leader>u", "<C-u>zz", { noremap = true })
 
 -- [ key bindings
 vim.keymap.set('n', '[', '<C-w>', { noremap = true })
