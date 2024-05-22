8147aa813d422ef36a239c47c1db2544502be57a
Debraj-Das <debrajdas.gayatri@gmail.com>
2024-05-22 12:56:09 +0530

    update the explorer setting

--- lua/plugins/explorer.lua
+++ lua/plugins/explorer.lua
@@ -8,6 +8,16 @@ return 	{
 	},
 	config = function()
 		vim.keymap.set({'n', 'v'}, '<leader>e', ':Neotree toggle<CR>', {})
+		require("neo-tree").setup({
+			filesystem = {
+				filtered_items = {
+					visible = false, -- when true, they will just be displayed differently than normal items
+					hide_dotfiles = false,
+					hide_gitignored = false,
+					hide_hidden = false, -- only works on Windows for hidden files/directories
+				}
+			}
+		})
 	end
 }
 
